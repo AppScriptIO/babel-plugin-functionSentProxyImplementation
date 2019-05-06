159364a498527ebb3b15289dc4f8118d51858054
Safi <sfmissive@gmail.com>
2019-05-06 22:26:02 +0300

    ⚗ Refactor for allowing manual build and release creation.

--- source/entrypoint/programmaticApi/transpilation.entrypoint.js
+++
@@ -1,6 +0,0 @@
-/* Entrypoint chain */
-// • Transpilation (babelJSCompiler)
-require('@dependency/javascriptTranspilation')({ babelConfigurationFile: 'serverRuntime.BabelConfig.js' })
-
-// • Run
-module.exports = require('./')
