d5aba6ff4b906ae7b1fa5f65c5edb97cc0912e95
Safi <sfmissive@gmail.com>
2019-05-06 22:37:42 +0300

    ➕ Update distribution files.

--- source/script.js
+++ source/script.js
@@ -1,65 +1,54 @@
-import { declare } from "@babel/helper-plugin-utils";
-import syntaxFunctionSent from "@babel/plugin-syntax-function-sent";
-import proxyWrapFunction from "./wrapFunctionWithProxy.js";
-import { types as t } from "@babel/core";
+import { declare } from '@babel/helper-plugin-utils'
+import syntaxFunctionSent from '@babel/plugin-syntax-function-sent'
+import proxyWrapFunction from './wrapFunctionWithProxy.js'
+import { types as t } from '@babel/core'
 
 export default declare(api => {
-  api.assertVersion(7);
+  api.assertVersion(7)
 
-  const isFunctionSent = node =>
-    t.isIdentifier(node.meta, { name: "function" }) &&
-    t.isIdentifier(node.property, { name: "sent" });
+  const isFunctionSent = node => t.isIdentifier(node.meta, { name: 'function' }) && t.isIdentifier(node.property, { name: 'sent' })
 
-  const hasBeenReplaced = (node, sentId) =>
-    t.isAssignmentExpression(node) &&
-    t.isIdentifier(node.left, { name: sentId });
+  const hasBeenReplaced = (node, sentId) => t.isAssignmentExpression(node) && t.isIdentifier(node.left, { name: sentId })
 
   const yieldVisitor = {
     Function(path) {
-      path.skip();
+      path.skip()
     },
 
     YieldExpression(path) {
       if (!hasBeenReplaced(path.parent, this.sentId)) {
-        path.replaceWith(
-          t.assignmentExpression("=", t.identifier(this.sentId), path.node),
-        );
+        path.replaceWith(t.assignmentExpression('=', t.identifier(this.sentId), path.node))
       }
     },
 
     MetaProperty(path) {
       if (isFunctionSent(path.node)) {
-        path.replaceWith(t.identifier(this.sentId));
+        path.replaceWith(t.identifier(this.sentId))
       }
     },
-  };
+  }
 
   return {
-    name: "proposal-function-sent",
+    name: 'proposal-function-sent',
     inherits: syntaxFunctionSent,
 
     visitor: {
       MetaProperty(path, state) {
-        if (!isFunctionSent(path.node)) return;
+        if (!isFunctionSent(path.node)) return
 
-        const fnPath = path.getFunctionParent();
+        const fnPath = path.getFunctionParent()
 
         if (!fnPath.node.generator) {
-          throw new Error("Parent generator function not found");
+          throw new Error('Parent generator function not found')
         }
 
-        const sentId = path.scope.generateUid("function.sent");
+        const sentId = path.scope.generateUid('function.sent')
 
-        fnPath.traverse(yieldVisitor, { sentId });
-        fnPath.node.body.body.unshift(
-          t.variableDeclaration("let", [
-            t.variableDeclarator(t.identifier(sentId), t.yieldExpression()),
-          ]),
-        );
+        fnPath.traverse(yieldVisitor, { sentId })
+        fnPath.node.body.body.unshift(t.variableDeclaration('let', [t.variableDeclarator(t.identifier(sentId), t.yieldExpression())]))
 
-        proxyWrapFunction(fnPath, state.addHelper("skipFirstGeneratorNext"));
+        proxyWrapFunction(fnPath, state.addHelper('skipFirstGeneratorNext'))
       },
     },
-  };
-  
-});
+  }
+})
